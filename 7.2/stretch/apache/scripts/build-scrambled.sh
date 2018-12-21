7e7b694f44922c9c71f627583f9ebc132391420a
Blue Gaston <bbluegaston@gmail.com>
2018-12-21 01:20:50 -0800

    fixed phar compile issue:

--- 7.2/stretch/apache/scripts/build-scrambled.sh
+++ 7.2/stretch/apache/scripts/build-scrambled.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-  
+
 ./php-scrambler
 
+cd $POLYSCRIPT_PATH
+s_php tok-php-transformer.php -p "$PHP_SRC_PATH"/ext/phar --replace --inc
+
 cd $PHP_SRC_PATH
 make install -k
 
 cd $POLYSCRIPT_PATH
-s_php tok-php-transformer.php -p "$PHP_SRC_PATH"ext/phar --replace --inc --phar
+s_php tok-php-transformer.php -p "$PHP_SRC_PATH"/ext/phar/phar.php --replace
 
 cd $PHP_SRC_PATH
 make install -k
-
-cd $POLYSCRIPT_PATH
-s_php tok-php-transformer.php -p "$PHP_SRC_PATH"ext/phar/phar.php --replace
-
-cd $PHP_SRC_PATH
-make pharcmd -k
