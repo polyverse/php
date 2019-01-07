5df518d152d9bbfa90df89e2bf561f43fcbf3ec8
Blue Gaston <bbluegaston@gmail.com>
2019-01-06 21:01:02 -0800

    fixed publish-image naming typo

--- 7.2/alpine3.8/fpm/publish-image.sh
+++ 7.2/alpine3.8/fpm/publish-image.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-image="polyverse/ps-php7.2-fpm-alpine"
+image="polyverse/ps-php7.2-fpm"
 
 echo "$(date) Obtaining current git sha for tagging the docker image"
 headsha=$(git rev-parse --verify HEAD)
 
 
-docker build -t $image:$headsha .
-docker push $image:$headsha
+docker build -t $image:alpine-$headsha .
+docker push $image:alpine-$headsha
 
 echo "Pushing as latest tag..."
-docker tag $image:$headsha $image:latest
+docker tag $image:alpine-$headsha $image:latest
 docker push $image:latest
 
