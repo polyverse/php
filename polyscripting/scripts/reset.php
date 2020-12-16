d0668bb273a7c909841de34a6d142f9f6ca55396
Blue Gaston <bbluegaston@gmail.com>
2020-12-16 14:41:26 -0800

    reset script for restoring vanilla php working

--- polyscripting/scripts/reset.php
+++ polyscripting/scripts/reset.php
@@ -1,4 +1,5 @@
 cp /usr/local/bin/polyscripting/vanilla-save/zend_language_scanner.l $PHP_SRC_PATH/Zend/zend_language_scanner.l
 cp  /usr/local/bin/polyscripting/vanilla-save/zend_language_parser.y $PHP_SRC_PATH/Zend/zend_language_parser.y
 cp /usr/local/bin/polyscripting/vanilla-save/phar.php $PHP_SRC_PATH/ext/phar/phar.php
-cp /usr/local/bin/polyscripting/vanilla-save/build-precommand.php $PHP_SRC_PATH/ext/phar/build-precommand.php
+cp /usr/local/bin/polyscripting/vanilla-save/build_precommand.php $PHP_SRC_PATH/ext/phar/build_precommand.php
+cd $PHP_SRC_PATH; make -j 1 install -k; cd $POLYSCRIPT_PATH;
