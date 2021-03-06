a53ebde2f0f3ef11d6511c551274f9a534d2c9ac
Blue Gaston <bbluegaston@gmail.com>
2021-03-05 19:00:17 -0700

    Adding gitflows test

--- polyscripting/build.sh
+++ polyscripting/build.sh
@@ -10,8 +10,13 @@ docker build -t $image:$headsha .
 docker tag $image:$headsha $image:latest
 
 
-if [[ "$1" == "-p" ]]; then
+if [[ "$1" == "-p" || "-a" ]]; then
 	docker push $image:$headsha
 	docker tag $image:$headsha $image:latest
         docker push $image:latest
+fi
+if [[ "$1" == "-g" || "-a" ]]; then
+	echo "Pushing to Github Container Repository"
+	docker tag $image:$headsha ghcr.io/$image:$headsha
+	docker push ghcr.io/$image:$headsha
 fi
--- polyscripting/tools/publish-image.sh
+++ polyscripting/tools/publish-image.sh
@@ -13,11 +13,17 @@ headsha=$(git rev-parse --verify HEAD)
 
 echo "Building image $image:$headsha"
 docker build -t $image:$headsha .
-docker push $image:$headsha
 
 
 if [[ "$1" == "-p" ]]; then
     echo "Pushing as latest tag..."
+	docker push $image:$headsha
     docker tag $image:$headsha $image:latest
     docker push $image:latest
+fi
+
+if if [[ "$1" == "-g" ]]; then
+	echo "Pushing to Github Container Repository"
+	docker tag $image:-$headsha ghcr.io/$image:$headsha
+	docker push ghcr.io/$image:$headsha
 fi
