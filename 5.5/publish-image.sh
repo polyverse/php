5a6dcb24f3a7bba664160cbdece5c69575436fe1
Blue Gaston <bbluegaston@gmail.com>
2021-03-17 21:44:01 -0700

    Removed sub git directory

---
+++ 5.5/publish-image.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Copyright (c) 2020 Polyverse Corporation
+
+type="$(basename $PWD)"
+build="$(basename $(dirname $PWD))"
+php_ver="$(basename $(dirname $(dirname $PWD)))"
+image="polyverse/ps-php${php_ver}-${build}-${type}"
+
+echo "Image $image being built"
+
+echo "$(date) Obtaining current git sha for tagging the docker image"
+headsha=$(git rev-parse --verify HEAD)
+
+echo "Building image $image:$headsha"
+docker build -t $image:$headsha .
+
+
+if [[ "$1" == "-p" ]]; then
+    echo "Pushing as latest tag..."
+	docker push $image:$headsha
+    docker tag $image:$headsha $image:latest
+    docker push $image:latest
+fi
+
+if if [[ "$1" == "-g" ]]; then
+	echo "Pushing to Github Container Repository"
+	docker tag $image:$headsha ghcr.io/$image:$headsha
+	docker push ghcr.io/$image:$headsha
+fi
